8b1317b09b048424d99296919eecfa90d0ae24bd
Johnny Hanbo <gony_han@hotmail.com>
2012-10-20 05:41:46 -0700

    delete

--- MainSolution/DataLayer/Services/Base/IService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ITGateWorkDesk.Data.DAO.Base;
-
-namespace ITGateWorkDesk.Data.Services.Base
-{
-    public interface IService<TDao, TEntity, TId> where TDao : IDao<TEntity, TId>
-    {
-       
-    }
-}
